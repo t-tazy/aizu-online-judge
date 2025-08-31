403d29a8206f4f4bdeccc2b62e125a39bc3ab3f8
t-tazy <takumin390511@gmail.com>
2025-08-31 22:54:56 +0900

    1TP1_1

---
+++ 1TP1/1_C.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int a, b, area, length;
+  cin >> a >> b;
+
+  area = a * b;
+  length = a * 2 + b * 2;
+
+  cout << area << ' ' << length << endl;
+
+  return 0;
+}
